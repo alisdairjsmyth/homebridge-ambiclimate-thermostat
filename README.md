91535f382beb8ddc029da71d366138a35672547d
alisdairjsmyth <alisdair.smyth@gmail.com>
2017-09-09 19:56:13 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # homebridge-ambiclimate-thermostat
-Ambi Climate Thermostat accessory plugin for Homebridge
+[![NPM Version](https://img.shields.io/npm/v/homebridge-ambiclimate-thermostat.svg)](https://www.npmjs.com/package/homebridge-ambiclimate-thermostat)
+[![Dependency Status](https://img.shields.io/versioneye/d/nodejs/homebridge-ambiclimate-thermostat.svg)](https://www.versioneye.com/nodejs/homebridge--thermostat/)
+
+[Ambi Climate](https://www.ambiclimate.com/) plugin for [homebridge](https://www.npmjs.com/package/homebridge) supporting Thermostat capabilities.  This plugin presently supports:
+* Current heating cooling state
+* Target heating cooling state
+* Current temperature
+* Target temperature
+* Current relative humidity
+
+## Installation
+
+    npm install -g homebridge-ambiclimate-thermostat
+
+This plugin augments a pre-existing implementation of [homebridge](https://www.npmjs.com/package/homebridge).  Refer to [nfarina/homebridge](https://www.npmjs.com/package/homebridge) for installation instructions.
+
+Register a OAuth Client in the <a href="https://api.ambiclimate.com/" target="_new">Ambi Dev Portal</a> for each Ambi Climate device by following the steps on the Quick Start page.  You require the Client Id and Client Secret of that client in order to use this wrapper.
+
+Update your homebridge configuration file (as below).
+
+## Configuration
+
+    "accessories" : [
+        {
+            "accessory": "AmbiClimateThermostat",
+            "name": "<Name for Accessory>",
+            "roomName": "<Name of Ambi Climate Device>",
+            "locationName": "<Name of Ambi Climate Location>",
+            "clientId": "<Ambi Climate OAuth Client Id>",
+            "clientSecret": "<Ambi Climate OAuth Client Secret>",
+            "username": "<Ambi Climate Username>",
+            "password": "<Ambi Climate Password>"
+        }
+    ]
+
+Separate homebridge accessories can be defined for each Ambi Climate device to be controlled.  
+* `accessory`: Must be "AmbiClimateThermostat"
+* `name`: Can be anything, this will be the name of the Accessory within HomeKit Apps
+* `room_name`: Must match the value within the Ambi Climate App
+* `location_name`: Must match the value within the Ambi Climate App
+* `clientId`: The Client Id value for the OAUTH Client obtained from Ambi Dev Portal
+* `clientSecret`: The Client Secret value for the OAUTH Client obtained from Ambi Dev Portal
+* `username`: Your Ambi Climate username
+* `password`: Your Ambi Climate password
+
+## To Do
+* Refactor to a Platform plugin.  This is predicated on Ambi Labs exposing a capability in their public API to retrieve all devices installed in a given location.
+* Implement Fan Service. This is predicated on Ambi Labs exposing a capability in their public API to get and set fan state.
---
+++ index.js
@@ -0,0 +1,246 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2017 Alisdair Smyth
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+**/
+var ambi = require('node-ambiclimate');
+var Service, Characteristic;
+
+module.exports = function(homebridge) {
+    Service        = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+    homebridge.registerAccessory("homebridge-ambiclimate-thermostat", "AmbiClimateThermostat", AmbiClimate);
+}
+
+function AmbiClimate(log, config) {
+    this.log                    = log;
+    this.name                   = config.name;
+    this.settings               = {};
+    this.settings.room_name     = config.roomName;
+    this.settings.location_name = config.locationName;
+
+    this.client = new ambi(config.clientId, config.clientSecret, config.username, config.password);
+
+    // Object to maintain the state of the device.  This is done as there is
+    // not official API to get this information.  If the Ambi Climate state is
+    // changed through an alternative channel, that change will not be reflected
+    // within Homekit
+    this.state              = {};
+    this.state.on           = false;
+
+    this.thermostatService = new Service.Thermostat(this.name);
+}
+
+AmbiClimate.prototype = {
+    getCurrentHeatingCoolingState: function(callback) {
+        //
+        // Map retrieved Ambi Climate mode to Homekit Heating, Cooling state.
+        // Comfort mode can be heating or cooling, but as there is no
+        // indicator of operating state this Ambi Climate mode is statically
+        // mapped to COOL.
+        // Temperature mode is mapped by comparing current temperature with
+        // the set temperature.
+        //
+        this.client.mode(this.settings, function(err, data) {
+            if (err) {
+                callback(err);
+            }
+            var mode     = data.mode;
+            var modeTemp = data.value;
+            this.log("Retrieved mode is " + mode);
+
+            switch(mode) {
+                case "Comfort":
+                case "Away_Temperature_Upper":
+                    callback(err, Characteristic.CurrentHeatingCoolingState.COOL);
+                case "Away_Temperature_Lower":
+                    callback(err, Characteristic.CurrentHeatingCoolingState.HEAT);
+                case "Temperature":
+                    this.client.sensor_temperature(accessory.settings, function (err, data) {
+                        if (err) {
+                          callback(err);
+                        }
+                        var temp = data[0].value;
+
+                        if (temp > modeTemp) {
+                          callback(err, Characteristic.CurrentHeatingCoolingState.COOL);
+                        } else {
+                          callback(err, Characteristic.CurrentHeatingCoolingState.HEAT);
+                        }
+                    })
+                default:
+                    callback(err, Characteristic.CurrentHeatingCoolingState.OFF);
+            }
+        })
+    },
+
+    //
+    // Target Heating Cooling State has the additional enumeration of AUTO.
+    // If the Ambi Climate is in the mode of Comfort or Temperature, the
+    // Target Heating Cooling State is set to AUTO.
+    //
+    getTargetHeatingCoolingState: function(callback) {
+      this.client.mode(this.settings, function(err, data) {
+          if (err) {
+              callback(err);
+          }
+          var mode     = data.mode;
+          var modeTemp = data.value;
+          this.log("Retrieved mode is " + mode);
+
+          switch(mode) {
+              case "Comfort":
+              case "Temperature":
+                  callback(err, Characteristic.TargetHeatingCoolingState.AUTO);
+              case "Away_Temperature_Upper":
+                  callback(err, Characteristic.TargetHeatingCoolingState.COOL);
+              case "Away_Temperature_Lower":
+                  callback(err, Characteristic.TargetHeatingCoolingState.HEAT);
+              default:
+                  callback(err, Characteristic.TargetHeatingCoolingState.OFF);
+          }
+      })
+    },
+
+    //
+    // AUTO -> Comfort
+    // COOL -> Away Temperature Upper
+    // HEAT -> Away Temperature Lower
+    //
+    setTargetHeatingCoolingState: function(value, callback) {
+        switch(value) {
+            case Characteristic.TargetHeatingCoolingState.AUTO:
+                this.client.comfort(this.settings, function(err, data) {
+                    callback(err, data);
+                }
+            case Characteristic.TargetHeatingCoolingState.COOL:
+                this.client.away_temperature_upper(this.settings, function(err, data) {
+                    callback(err, data);
+                }
+            case Characteristic.TargetHeatingCoolingState.HEAT:
+                this.client.away_temperature_lower(this.settings, function(err, data) {
+                    callback(err, data);
+                }
+            default:
+                this.client.off(this.settings, function(err, data) {
+                    callback(err, data);
+                }
+        }
+    },
+
+    getCurrentTemperature: function(callback) {
+        accessory.client.sensor_temperature(accessory.settings, function (err, data) {
+            (err) ? callback(err, null) : callback(err, data[0].value);
+        });
+    },
+
+    getTargetTemperature: function(callback) {
+      this.client.mode(this.settings, function(err, data) {
+          if (err) {
+              callback(err);
+          }
+          var mode     = data.mode;
+          var modeTemp = data.value;
+
+          switch(mode) {
+              case "Temperature":
+              case "Away_Temperature_Upper":
+              case "Away_Temperature_Lower":
+                  callback(null, modeTemp);
+              default:
+                  callback(null, 22);
+          }
+    },
+
+    //
+    // Set the target temperature using the current Ambi Climate mode. A
+    // target temperature is only relevant for Temperature, and Away
+    // Temperature modes.
+    //
+    setTargetTemperature: function(value, callback) {
+        this.client.mode(this.settings, function(err, data) {
+            if (err) {
+                callback(err);
+            }
+            var mode     = data.mode;
+
+            this.client.settings.value = value;
+            switch(mode) {
+                case "Temperature":
+                    this.client.temperature(this.client.settings.value, function(err,data) {
+                        callback(err);
+                    });
+                case "Away_Temperature_Upper":
+                    this.client.away_temperature_upper(this.client.settings.value, function(err,data) {
+                        callback(err);
+                    });
+                case "Away_Temperature_Lower":
+                    this.client.away_temperature_lower(this.client.settings.value, function(err,data) {
+                        callback(err);
+                    });
+                default:
+                    callback(null);
+            }
+        }
+    },
+
+    getTemperatureDisplayUnits: function(callback) {
+        callback(null, 0);
+    },
+
+    getCurrentRelativeHumidity: function(callback) {
+        this.client.sensor_humidity(this.settings, function (err, data) {
+            (err) ? callback(err) : callback(err, data[0].value);
+        });
+    },
+
+    //
+    // Services
+    //
+    getServices: function () {
+        this.thermostatService.getCharacteristic(Characteristic.CurrentHeatingCoolingState)
+            .on('get', this.getCurrentHeatingCoolingState.bind(this));
+
+        this.thermostatService.getCharacteristic(Characteristic.TargetHeatingCoolingState)
+            .on('get', this.getTargetHeatingCoolingState.bind(this))
+            .on('set', this.setTargetHeatingCoolingState.bind(this));
+
+        this.thermostatService.getCharacteristic(Characteristic.CurrentTemperature)
+            .on('get', this.getCurrentTemperature.bind(this));
+
+        this.thermostatService.getCharacteristic(Characteristic.TargetTemperature)
+            .on('get', this.getTargetTemperature.bind(this))
+            .on('set', this.setTargetTemperature.bind(this));
+
+        this.thermostatService.getCharacteristic(Characteristic.TemperatureDisplayUnits)
+            .on('get', getTemperatureDisplayUnits.bind(this));
+
+        this.thermostatService.getCharacteristic(Characteristic.CurrentRelativeHumidity)
+            .on('get', getCurrentRelativeHumidity.bind(this));
+
+     		this.informationService
+    			.setCharacteristic(Characteristic.Manufacturer, "Ambi Labs")
+    			.setCharacteristic(Characteristic.Model, "Ambi Climate Thermostat")
+    			.setCharacteristic(Characteristic.SerialNumber, " ");
+
+        return [this.thermostatService];
+    }
+}
